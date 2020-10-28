673ac247cb2b2b3d6da43d0d24e50b7146168a6e
Ryuzakighost <ryuzakighost@gmail.com>
2020-10-28 17:17:10 +1100

    added MSG manager, Started Setup task

--- Index.js
+++ Index.js
@@ -2,78 +2,111 @@
 //Imports
 const discord = require('discord.js')
 const queue = require('./queue.js')
-//require('dotenv').config();
+const { token,prefix } = require('./config.json')
+//require('dotenv-flow').config()
 
 //Const + Var Values 
-const token = "NzY5MDQ2MDU3NzUyODU0NTM4.X5JT9A.3p-4fhKp_3QoUAYIKeaPk-bVWvk"//token.env.TOKEN;
 const bot = new discord.Client()
 let playerQueue = new queue.Queue()
 const permissionsError = "You have insufficient permissions to use this command"
 var prevMessage = ""
 var prevMessageChannel = ""
+var guildID = null
+
 
 bot.on('ready', () => {
 console.info(`Logged in as ${bot.user.tag}!`)
+//Sets the GuildID
+
+//check for game room channel
+//if one exists note channel ID, else make a game room channel
+
+//check for mod role
+//if mod role doesn't exist, create it  
 })
 
 bot.login(token)
 
 bot.on('message', msg => {
+    //checks to see if message begins with prefix
+    if(msg.content.startsWith(prefix)){
 
-    /*Enqueue function
-    - adds user to the queue if they don't exist in it
-    - removes user from the queue if they are already in it*/
-    if (msg.content === '!q'||msg.content === '!queue') {
-        user = msg.author
-        status = playerQueue.enqueue(user.id)
-        if(status === "added"){
-            printQueue(msg.channel,'User: <@!' + user.id +'> has been added to the queue')
+        //if guildID isn't set, set it
+        if(guildID === null){
+            guildID = msg.guild.id
+            console.log("Guild ID set: " + guildID)
         }
-        else if(status === "removed"){
-            printQueue(msg.channel,'User: <@!' + user.id +'> has been removed from the queue')
+        /*Setup function
+        This function is the setup for the voice chat function
+        It does as follows:
+        - Checks for a game room channel
+             - if it doesn't exist create it
+        - Checks for mod role
+             - if it doesn't exist create it*/
+        if (msg.content === prefix + 'setup'){
+            
         }
-        else{
-            console.error('something went wrong adding user to queue')
-        }
-        msg.delete()
-        
-    }
 
-    /*Kick function
-    - kicks user from the queue
-    - This is a Mod Command*/
-    else if(msg.content.startsWith('!k ')) {
-        if(msg.member.roles.cache.some(role => role.name === 'Mod')){
-            id = msg.content.slice(3)
-            kick(id,msg.channel)
+        /*Enqueue function
+        - adds user to the queue if they don't exist in it
+        - removes user from the queue if they are already in it*/
+        if (msg.content === prefix + 'q'||msg.content === prefix + 'queue') {
+            user = msg.author
+            status = playerQueue.enqueue(user.id)
+            if(status === "added"){
+                printQueue(msg.channel,'User: <@!' + user.id +'> has been added to the queue')
+            }
+            else if(status === "removed"){
+                printQueue(msg.channel,'User: <@!' + user.id +'> has been removed from the queue')
+            }
+            else{
+                console.error('something went wrong adding user to queue')
+            }
             msg.delete()
+            
         }
-        else{
-            console.error('permissions error for ' + msg.author.username)
-            msg.reply(permissionsError)
-            msg.delete()
-        }
-    }
-    else if(msg.content.startsWith('!kick ')) {
-        if(msg.member.roles.cache.some(role => role.name === 'Mod')){
-            id = msg.content.slice(6)
-            kick(id,msg.channel)
-            msg.delete()
-        }
-        else{
-            console.error('permissions error for ' + msg.author.username)
-            msg.reply(permissionsError)
-            msg.delete()
-        }
-    }
 
-    /*Print Function
-    - prints the current queue in chat*/
-    else if(msg.content === '!p' || msg.content === '!print'){
-        msg.delete()
-        printQueue(msg.channel,"")
-        console.log("Queue Printed")
-        
+        /*Kick function
+        - kicks user from the queue
+        - This is a Mod Command*/
+        else if(msg.content.startsWith(prefix + 'k ')) {
+            if(msg.member.roles.cache.some(role => role.name === 'Mod')){
+                id = msg.content.slice(3)
+                kick(id,msg.channel)
+                msg.delete()
+            }
+            else{
+                console.error('permissions error for ' + msg.author.username)
+                msg.reply(permissionsError)
+                msg.delete()
+            }
+        }
+        else if(msg.content.startsWith(prefix + 'kick ')) {
+            if(msg.member.roles.cache.some(role => role.name === 'Mod')){
+                id = msg.content.slice(6)
+                kick(id,msg.channel)
+                msg.delete()
+            }
+            else{
+                console.error('permissions error for ' + msg.author.username)
+                msg.reply(permissionsError)
+                msg.delete()
+            }
+        }
+
+        /*Next Function
+        - Starts messaging down the queue - direct messaging
+        - Each player gets 1 min to respond to the message before it moves onto the next
+        - once a user joins if no space left stop
+
+        /*Print Function
+        - prints the current queue in chat*/
+        else if(msg.content === prefix + 'p' || msg.content === prefix + 'print'){
+            msg.delete()
+            printQueue(msg.channel,"")
+            console.log("Queue Printed")
+            
+        }
     }
 })
 
@@ -85,8 +118,12 @@ bot.on("voiceStateUpdate", (oldMember, newMember)=> {
     if (oldVoice != newVoice) {
       if (oldVoice == null) {
         console.log("User joined!")
+        //remove them from queue
+
       } else if (newVoice == null) {
         console.log("User left!")
+        //find new user if list not empty
+
       } else {
         console.log("User switched channels!")
       }
@@ -108,7 +145,6 @@ async function kick(username,channel){
 
 //See Print Function
 async function printQueue(channel, msg = ""){
-    console.log(msg)
     printString = msg + "\n>>> Queue:\n"
     if(playerQueue.queue.length === 0){
         printString = msg + "\n>>> There is no-one here\n"
@@ -124,7 +160,6 @@ async function printQueue(channel, msg = ""){
 
 //sends message and deletes previous message
 async function msgManager(msg,channel) {
-    console.log("prevMessage: " + prevMessage)
     if(prevMessage != ""){
         //delete previous message
         bot.channels.cache.get(prevMessageChannel).messages.fetch(prevMessage).then(message => message.delete())
