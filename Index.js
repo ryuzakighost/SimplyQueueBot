b69b5930174026a908825575e806556d204cb3a0
ryuzakighost <ryuzakighost@gmail.com>
2020-10-30 00:52:56 +1100

    now detecting when a user enters a channel

--- Index.js
+++ Index.js
@@ -1,8 +1,12 @@
 
 //Imports
 const discord = require('discord.js')
-const queue = require('./queue.js')
-const { token,prefix } = require('./config.json')
+const queue = require('./models/queue.js')
+const channel = require('./models/channel.js')
+const config = require('./config.json')
+//const command = require('./src/commands.js')
+//const utility = require('./src/utility.js')
+var fs = require('fs');
 //require('dotenv-flow').config()
 
 //Const + Var Values 
@@ -11,65 +15,75 @@ let playerQueue = new queue.Queue()
 const permissionsError = "You have insufficient permissions to use this command"
 var prevMessage = ""
 var prevMessageChannel = ""
-var guildID = null
+var guild = null
+var gameChannels = []
+var playerFind = false
 
 
+//Startup commands for the Bot
 bot.on('ready', () => {
-console.info(`Logged in as ${bot.user.tag}!`)
-//Sets the GuildID
-
-//check for game room channel
-//if one exists note channel ID, else make a game room channel
+    console.info("Getting Bot Ready")
+    //Sets the Guild and sets up channels
+    if (config.guild != ""){
+        guild = bot.guilds.cache.get(config.guild)
+        console.info("Guild " + guild.name + " loaded")
+        addChannel(config.gameRoomName + " 1")
+    }
 
 //check for mod role
 //if mod role doesn't exist, create it  
+    console.info(`Logged in as ${bot.user.tag}!\n`)
 })
 
-bot.login(token)
+bot.login(config.token)
 
 bot.on('message', msg => {
     //checks to see if message begins with prefix
-    if(msg.content.startsWith(prefix)){
+    if(msg.content.startsWith(config.prefix)){
 
         //if guildID isn't set, set it
-        if(guildID === null){
-            guildID = msg.guild.id
-            console.log("Guild ID set: " + guildID)
-        }
-        /*Setup function
-        This function is the setup for the voice chat function
-        It does as follows:
-        - Checks for a game room channel
-             - if it doesn't exist create it
-        - Checks for mod role
-             - if it doesn't exist create it*/
-        if (msg.content === prefix + 'setup'){
-            
+        if(config.guild === ""){
+            config.guild = msg.guild.id
+            fs.writeFile('./config.json',JSON.stringify(config),"utf8",(err) => {
+                if (err) throw err
+                console.log('Successfully written to config.json')})
+            console.log("Guild ID set: " + config.guild)
+            addChannel(config.gameRoomName + " 1")
         }
 
         /*Enqueue function
         - adds user to the queue if they don't exist in it
         - removes user from the queue if they are already in it*/
-        if (msg.content === prefix + 'q'||msg.content === prefix + 'queue') {
+        if (msg.content === config.prefix + 'q'||msg.content === config.prefix + 'queue') {
             user = msg.author
-            status = playerQueue.enqueue(user.id)
-            if(status === "added"){
-                printQueue(msg.channel,'User: <@!' + user.id +'> has been added to the queue')
-            }
-            else if(status === "removed"){
-                printQueue(msg.channel,'User: <@!' + user.id +'> has been removed from the queue')
+            
+            isInChat = false
+            gameChannels.forEach(function(voiceChannel){
+                if(voiceChannel.channel.members.has(user.id)){
+                    isInChat = true
+                }
+            })
+            if(isInChat === false){
+                if(status === "added"){
+                    printQueue(msg.channel,'User: <@!' + user.id +'> has been added to the queue')
+                }
+                else if(status === "removed"){
+                    printQueue(msg.channel,'User: <@!' + user.id +'> has been removed from the queue')
+                }
+                else{
+                    console.error('something went wrong adding user to queue')
+                } 
             }
             else{
-                console.error('something went wrong adding user to queue')
+                msg.author.send("Hi <@!" + user.id +">, You can't add yourself to Queue if you are in a " + config.gameRoomName)
             }
             msg.delete()
-            
         }
 
         /*Kick function
         - kicks user from the queue
         - This is a Mod Command*/
-        else if(msg.content.startsWith(prefix + 'k ')) {
+        else if(msg.content.startsWith(config.prefix + 'k ')) {
             if(msg.member.roles.cache.some(role => role.name === 'Mod')){
                 id = msg.content.slice(3)
                 kick(id,msg.channel)
@@ -81,7 +95,7 @@ bot.on('message', msg => {
                 msg.delete()
             }
         }
-        else if(msg.content.startsWith(prefix + 'kick ')) {
+        else if(msg.content.startsWith(config.prefix + 'kick ')) {
             if(msg.member.roles.cache.some(role => role.name === 'Mod')){
                 id = msg.content.slice(6)
                 kick(id,msg.channel)
@@ -89,7 +103,7 @@ bot.on('message', msg => {
             }
             else{
                 console.error('permissions error for ' + msg.author.username)
-                msg.reply(permissionsError)
+                msg.author.send(permissionsError)
                 msg.delete()
             }
         }
@@ -101,7 +115,7 @@ bot.on('message', msg => {
 
         /*Print Function
         - prints the current queue in chat*/
-        else if(msg.content === prefix + 'p' || msg.content === prefix + 'print'){
+        else if(msg.content === config.prefix + 'p' || msg.content === config.prefix + 'print'){
             msg.delete()
             printQueue(msg.channel,"")
             console.log("Queue Printed")
@@ -113,8 +127,16 @@ bot.on('message', msg => {
 bot.on("voiceStateUpdate", (oldMember, newMember)=> { 
     let oldVoice = oldMember.channelID
     let newVoice = newMember.channelID
-    console.log(oldVoice)
-    console.log(newVoice)
+    //if user enters a voice chat
+    gameChannels.forEach(function(channel){
+        console.log(newVoice)
+        console.log(channel.channel.id)
+        if(newVoice === channel.channel.id && playerQueue.queue.includes(newMember.id)){
+            kick("<@!"+newMember.id+">",guild.channels.cache.get(config.defaultChat))
+        }
+    })
+    //console.log(oldVoice)
+    //console.log(newVoice)
     if (oldVoice != newVoice) {
       if (oldVoice == null) {
         console.log("User joined!")
@@ -168,4 +190,23 @@ async function msgManager(msg,channel) {
         prevMessage = sent.id
         prevMessageChannel = channel.id
     })
+}
+
+//check for game room channel
+//if one exists note channel ID, else make a game room channel
+async function addChannel(channelName){
+    if(!(guild.channels.cache.find(c => c.name === channelName))){
+        console.log("Creating " + channelName)
+        guild.channels.create(channelName,{type: 'voice', userLimit: '10' })
+            .then(createdChannel => {
+                console.log(createdChannel.name + " Created")
+                gameChannels.push(new channel.Channel(createdChannel,true))
+            })
+            .catch(console.error)
+    }
+    else {
+        fetchedChannel = guild.channels.cache.find(c => c.name === channelName)
+        gameChannels.push(new channel.Channel(fetchedChannel,false))
+        console.log("Channel Found " + channelName)
+    }
 }
---
+++ models/channel.js
@@ -0,0 +1,7 @@
+class Channel {
+    constructor(channel,isDelete){
+        this.channel = channel
+        this.isDelete = isDelete
+    }
+    
+}module.exports.Channel = Channel;
